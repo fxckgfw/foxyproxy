353a428775e2f92dc600ef93efd445879a7694eb
grimholt <grimholt@560d4a0b-fb2f-0410-bc46-b98b48829bb1>
2008-02-13 02:57:31 +0000

    more work towards SongBird support
    git-svn-id: file:///opt/dev/work/svn/svn/basic_and_standard/trunk@57 560d4a0b-fb2f-0410-bc46-b98b48829bb1

--- src/content/optionsOverlay.js
+++ src/content/optionsOverlay.js
@@ -1,3 +1,13 @@
+/**
+  FoxyProxy
+  Copyright (C) 2006-2008 Eric H. Jung and LeahScape, Inc.
+  http://foxyproxy.mozdev.org/
+  eric.jung@yahoo.com
+
+  This source code is released under the GPL license,
+  available in the LICENSE file at the root of this installation
+  and also online at http://www.gnu.org/licenses/gpl.txt
+**/
 window.onload=function(){
   // works with ff 1.5, 2.x, and 3.x
   var e = document.getElementById("catProxiesButton") || document.getElementById("connectionSettings");
@@ -6,25 +16,22 @@ window.onload=function(){
 	  try {
 	    gAdvancedPane && (gAdvancedPane.showConnections = onConnectionSettings);
 	  }
-	  catch (e) {/*wtf*/}
+	  catch (e) {dump(e);/*wtf*/}
 	}
 }
 function onConnectionSettings() {
-
   var fp = Components.classes["@leahscape.org/foxyproxy/service;1"]
     .getService(Components.interfaces.nsISupports).wrappedJSObject;
   
   if (fp.mode == "disabled")
 	  document.documentElement.openSubDialog("chrome://browser/content/preferences/connection.xul", "", null);
 	else {
-	  var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]
-	          .getService(Components.interfaces.nsIWindowMediator);
-		var win = wm.getMostRecentWindow("navigator:browser");
+		var win = foxyproxy_common.getMostRecentWindow();
 		if (win && win.foxyproxy)
 		  win.foxyproxy.onOptionsDialog();
 		else {
 		  alert("FoxyProxy Error");
-			document.documentElement.openSubDialog("chrome://browser/content/preferences/connection.xul", "", null);		  
+		  document.documentElement.openSubDialog("chrome://browser/content/preferences/connection.xul", "", null);		  
 		}
 	}
 }

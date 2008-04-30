21b13abd2813e17f083d1da5ac8aaed72d448716
grimholt <grimholt@560d4a0b-fb2f-0410-bc46-b98b48829bb1>
2008-04-30 02:35:29 +0000

    git-svn-id: file:///opt/dev/work/svn/svn/basic_and_standard/trunk@116 560d4a0b-fb2f-0410-bc46-b98b48829bb1

--- src/content/pattern.js
+++ src/content/pattern.js
@@ -8,30 +8,31 @@
   available in the LICENSE file at the root of this installation
   and also online at http://www.gnu.org/licenses/gpl.txt
 **/
-
-var overlay;
-
 function onOK() {
-  var r = document.getElementById("matchtype").value == "r";
-  var p = Components.classes["@leahscape.org/foxyproxy/common;1"].getService().wrappedJSObject.validatePattern(window, r, document.getElementById("pattern").value);
+  var r = document.getElementById("matchtype").value == "r",
+    pattern = document.getElementById("pattern").value;
+  var p = Components.classes["@leahscape.org/foxyproxy/common;1"].getService()
+    .wrappedJSObject.validatePattern(window, r, pattern);
   if (p) {
 	  window.arguments[0].out = {name:document.getElementById("name").value,
 	    pattern:p, isRegEx:r,
 	    isBlackList:document.getElementById("whiteblacktype").value == "b",
 	    isEnabled:document.getElementById("enabled").checked,
-      caseSensitive:document.getElementById("caseSensitive").checked};
+      caseSensitive:document.getElementById("caseSensitive").checked,
+      temp:document.getElementById("temp").checked};
 	  return true;
 	}
   return false;
 }
 
 function onLoad() {
-  overlay = window.arguments[0].inn.overlay;
-  document.getElementById("name").value = window.arguments[0].inn.name;
-  document.getElementById("pattern").value = window.arguments[0].inn.pattern;
-  document.getElementById("matchtype").selectedIndex = window.arguments[0].inn.regex ? 1 : 0;
-  document.getElementById("whiteblacktype").selectedIndex = window.arguments[0].inn.black ? 1 : 0;
-  document.getElementById("enabled").checked = window.arguments[0].inn.enabled;
-  document.getElementById("caseSensitive").checked = window.arguments[0].inn.caseSensitive;
+  var inn = window.arguments[0].inn;
+  document.getElementById("name").value = inn.name;
+  document.getElementById("pattern").value = inn.pattern;
+  document.getElementById("matchtype").selectedIndex = inn.regex ? 1 : 0;
+  document.getElementById("whiteblacktype").selectedIndex = inn.black ? 1 : 0;
+  document.getElementById("enabled").checked = inn.enabled;
+  document.getElementById("caseSensitive").checked = inn.caseSensitive;
+  document.getElementById("temp").checked = inn.temp;  
   sizeToContent();
 }

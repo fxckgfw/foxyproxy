3f0baa60884a81ffafcf6a3f43e1a1a0a6a50cd5
grimholt <grimholt@560d4a0b-fb2f-0410-bc46-b98b48829bb1>
2009-04-20 20:29:18 +0000

    git-svn-id: file:///opt/dev/work/svn/svn/basic_and_standard/trunk@338 560d4a0b-fb2f-0410-bc46-b98b48829bb1

---
+++ src/content/chooseproxy.js
@@ -0,0 +1,21 @@
+var proxyTree, fp, fpc, overlay;
+const CC = Components.classes, CI = Components.interfaces;
+
+function onLoad() {
+  proxyTree = document.getElementById("proxyTree");
+  fp = CC["@leahscape.org/foxyproxy/service;1"].getService().wrappedJSObject;  
+  fpc = CC["@leahscape.org/foxyproxy/common;1"].getService().wrappedJSObject;
+  overlay = fpc.getMostRecentWindow().foxyproxy;
+  var inn = window.arguments[0].inn;
+  document.getElementById("title").value = inn.title;
+  proxyTree.view = fpc.makeProxyTreeView(fp);
+  proxyTree.view.selection.select(0); /* select the first entry */
+  sizeToContent();
+}
+
+function onOK() {
+  if (proxyTree.currentIndex != -1)
+    window.arguments[0].out = {proxy:fp.proxies.item(proxyTree.currentIndex), reloadcurtab:
+      document.getElementById("reloadcurtab").checked};
+  return true;
+}

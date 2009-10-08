45fc853db6848e452357b1a0be00c788701a145f
ejung <eric.jung@yahoo.com>
2009-10-08 13:31:30 +0000

    More work towards FoxyProxy Simple

--- src/defaults/preferences/prefs.js
+++ src/defaults/preferences/prefs.js
@@ -1,2 +1,3 @@
 // See http://kb.mozillazine.org/Localize_extension_descriptions
 pref("extensions.foxyproxy@eric.h.jung.description", "chrome://foxyproxy/locale/foxyproxy.properties");
+pref("extensions.foxyproxy-simple@eric.h.jung.description", "chrome://foxyproxy/locale/foxyproxy.properties");

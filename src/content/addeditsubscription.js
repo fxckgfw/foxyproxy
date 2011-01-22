0779ef34c6a68d5724a82101d3afde9731e3453d
gkoppen <gkoppen@560d4a0b-fb2f-0410-bc46-b98b48829bb1>
2011-01-22 13:35:31 +0000

    finished work on pattern subscriptions XUL
    git-svn-id: file:///opt/dev/work/svn/svn/basic_and_standard/trunk@720 560d4a0b-fb2f-0410-bc46-b98b48829bb1

--- src/content/addeditsubscription.js
+++ src/content/addeditsubscription.js
@@ -1,1 +1,36 @@
-"use strict;"
+"use strict";
+
+function onLoad() {
+   
+}
+
+function onOK() {
+
+}
+
+function onCancel() {
+
+}
+
+function onLastStatus() {
+  window.openDialog('chrome://foxyproxy/content/laststatus.xul', '', 
+      'modal, centerscreen, resizable').focus();
+}
+
+function addProxy() {
+  var Cc = Components.classes;
+  var fp = Cc["@leahscape.org/foxyproxy/service;1"].getService().
+             wrappedJSObject; 
+  var p = {
+    inn: {
+      title: fp.getMessage("choose.proxy.patterns"), 
+      pattern: true
+    }, 
+    out: null
+  };          
+  window.openDialog("chrome://foxyproxy/content/chooseproxy.xul", "",
+        "modal, centerscreen, resizable", p).focus(); 
+  if (p.out) {
+    p = p.out;
+  }
+}

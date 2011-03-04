8ff609b8ef7f4d0e343fcedb2115aace50d8c015
gkoppen <georg@getfoxyproxy.org>
2011-03-04 09:03:18 +0000

    added lastStatus functionality (errorMessages and copy to clipboard are missing yet); bugfixing in subscriptionsTree

--- src/content/pattern-subscriptions/laststatus.js
+++ src/content/pattern-subscriptions/laststatus.js
@@ -11,6 +11,47 @@
 
 "use strict";
 
+function onLoad() {
+  var errorMessages = [], status, i, treeChildren = []; 
+  status = window.arguments[0].inn.status;
+  if (window.arguments[0].inn.errorMessages) {
+    errorMessages = window.arguments[0].inn.errorMessages;
+  }
+  // Preparing the treeChildren to simplify the logic needed in the view 
+  // itself.
+  treeChildren.push(status);
+  if (errorMessages) {
+    treeChildren.push("");
+    for (i = 0; i < errorMessages.length; i++) {
+      treeChildren.push(errorMessages[i]);
+    } 
+  }
+  var statusTree = document.getElementById("lastStatusTree");
+  statusTree.view = {
+    get rowCount() {
+      if (!errorMessages) {
+        return 1;
+      } else {
+        // One additional line for the status message and one is an empty line
+        // that separates the status message from the error messages.
+        return errorMessages.length + 2;
+      }
+    },
+    getCellText : function(row,column){
+      return treeChildren[row];
+    },
+    setTree: function(treebox){},
+    getColumnProperties: function(col, elem, prop){},
+    isSorted: function(){},
+    isContainer: function(index){return false},
+    isSeparator: function(index){return false},
+    getRowProperties: function(index, prop){},
+    getCellProperties: function(row, col, prop){},
+    getImageSrc: function(row, col){},
+    cycleHeader: function(col){}  
+  };
+}
+
 function onOK() {
 
 }

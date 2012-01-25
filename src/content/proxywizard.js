6c42ad07bf8e726d1e77a76d1f4dc103f012e0c6
Georg Koppen <georg@getfoxyproxy.org>
2012-01-25 15:21:25 +0000

    implemented error handling in proxy wizard; small code clean-up in patsub code

--- src/content/proxywizard.js
+++ src/content/proxywizard.js
@@ -16,26 +16,40 @@ function onOK() {
   let url = "https://getfoxyproxy.org/proxyservice/get-details-fp.php?subscription="
   let subscriptionID = document.getElementById("subscriptionID").value; 
   let req = new XMLHttpRequest();
+  // We want to be able to let the dialog opened it the user just had a typo in
+  // her entered ID. Unfortunately that does not work with a simple "return
+  // false;" statement from within the onreadystatechange function. That's
+  // why we use a new property, set it to false if needed and return its value
+  // instead.
+  req.retValue = true;  
   req.onreadystatechange = function (oEvent) {
     if (req.readyState === 1) {
+      // Let's show the user that we are fetching her proxy details.
       document.getElementById("loadHint").collapsed = false;
       sizeToContent();
     } else if (req.readyState === 4) {
       document.getElementById("loadHint").collapsed = true;
       sizeToContent();
       if (req.status === 200) {
-        // We got something back. Let's try to create a proxy-URL and parse it.
-        let fpc = Components.classes["@leahscape.org/foxyproxy/common;1"].
-          getService().wrappedJSObject;
-        try {
-          proxyURI = fpc._ios.newURI(req.responseText, null, null);
-        } catch(e) {
-          // We could not generate a URI. Thus, parsing of the proxy details
-          // will fail...
-          fp.alert(null, fp.getMessage("proxywiz.parse.failure"));
-          return;
+        let response = req.responseText;
+        // We got something back. Let's try to create a proxy-URL and parse it
+        // if it is not an "error" message.
+        if (response !== "error") {
+          let fpc = Components.classes["@leahscape.org/foxyproxy/common;1"].
+            getService().wrappedJSObject;
+          try {
+            proxyURI = fpc._ios.newURI(req.responseText, null, null);
+          } catch(e) {
+            // We could not generate a URI. Thus, parsing of the proxy details
+            // will fail...
+          }
+          fpc.processProxyURI(proxyURI);
+        } else {
+          // The user entered an invalid subscription id
+          fp.alert(null, fp.getMessage("proxywiz.id.failure"));
+          // Let the dialog open to make it easier to correct just a typo.
+          this.retValue = false; 
         }
-        fpc.processProxyURI(proxyURI);
       } else {
         fp.alert(null, fp.getMessage("proxywiz.load.failure"));     
       }
@@ -43,5 +57,5 @@ function onOK() {
   }
   req.open("GET", url + subscriptionID, false);
   req.send(null);
-  return true;
+  return req.retValue;
 }

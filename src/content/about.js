665397a4326647309166cc1ad9d68959a5d1d32f
ejung <ejung@560d4a0b-fb2f-0410-bc46-b98b48829bb1>
2012-02-06 01:17:48 +0000

    work on ticket 221
    git-svn-id: file:///opt/dev/work/svn/svn/basic_and_standard/gecko/trunk@1032 560d4a0b-fb2f-0410-bc46-b98b48829bb1

--- src/content/about.js
+++ src/content/about.js
@@ -11,7 +11,7 @@
 var fpc, intervalId, angle = 4, iconRotater;
 
 function onLoad() {
-  document.documentElement.getButton("accept").focus()
+  document.documentElement.getButton("accept").focus();
   fpc = Components.classes["@leahscape.org/foxyproxy/common;1"].getService().wrappedJSObject;
   document.getElementById("ver").value += " " + fpc.getVersion();
 	sizeToContent();  

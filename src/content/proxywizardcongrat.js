48157db40336364221d88c55db5d405c0100dc4a
ejung <ejung@560d4a0b-fb2f-0410-bc46-b98b48829bb1>
2012-02-09 05:10:57 +0000

    Proxy wizard: password, if provided by the web page or JSON, is now displayed in the congratulations dialog
    git-svn-id: file:///opt/dev/work/svn/svn/basic_and_standard/gecko/trunk@1036 560d4a0b-fb2f-0410-bc46-b98b48829bb1

--- src/content/proxywizardcongrat.js
+++ src/content/proxywizardcongrat.js
@@ -23,9 +23,12 @@ function onLoad() {
       .wrappedJSObject;
   if (inn && inn.country)
     var msg = fp.getMessage("proxy.wizard.getfoxyproxy", [inn.country, inn.
-      username]);
+      username,
+      // The password may not be present. If not, use empty string. Otherwise,
+      // use e.g., "(secret)"
+      inn.password ? ("(" + inn.password + ")") : ""]);
   else
-    var msg = fp.getMessage("proxy.wizard.getfoxyproxy", ["?", "?"]);
+    var msg = fp.getMessage("proxy.wizard.getfoxyproxy", ["?", "?", "?"]);
   let msg2 = document.createTextNode(msg);
   document.getElementById("msg").appendChild(msg2);
   document.documentElement.getButton("accept").focus();

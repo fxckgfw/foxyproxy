704a6682db4cba478ae7ec39ea52f5274ac7a546
Eric Jung <eric.jung@getfoxyproxy.org>
2012-02-15 07:36:31 +0000

    fixing typo in IDL compiler script

--- compile-idl.sh
+++ compile-idl.sh
@@ -5,4 +5,4 @@ CACHE_PATH=/tmp
 OUTPUT_PATH=/home/fidel/dev/foxyproxy/basic_and_standard/trunk/src/components
 INPUT_PATH=/home/fidel/dev/foxyproxy/basic_and_standard/trunk/src/components
 
-$XULRUNNER_SDK_PATH/sdk/bin/typelib.py -I $XULRUNNER_SDK_PATH/idl/ --cachedir $CACH_PATH -o $OUTPUT_PATH/api.xpt $INPUT_PATH/api.idl
+$XULRUNNER_SDK_PATH/sdk/bin/typelib.py -I $XULRUNNER_SDK_PATH/idl/ --cachedir $CACHE_PATH -o $OUTPUT_PATH/api.xpt $INPUT_PATH/api.idl

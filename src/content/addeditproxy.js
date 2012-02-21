b7c64be90c3f9bc074f3cd36818fd926119cbd8c
gkoppen <georg@getfoxyproxy.org>
2012-02-21 11:19:50 +0000

    fixed some small bugs in addeditproxy.js; made layout more uniform

--- src/content/addeditproxy.js
+++ src/content/addeditproxy.js
@@ -9,13 +9,14 @@
   and also online at http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 **/
 
-const CI = Components.interfaces, CC = Components.classes, CU =Components.utils;
+const CI = Components.interfaces, CC = Components.classes, CU = Components.
+  utils;
 var urlsTree, proxy, foxyproxy, autoconfUrl, overlay, isWindows, fpc,
   autoconfMode, reloadFreq, loadNotification, errorNotification, autoReload,
   oldMatches = [];
 
 CU.import("resource://foxyproxy/cookiesAndCache.jsm");
-CU.import("resource://foxyproxy/patternSubscriptions.jsm");
+
 function onLoad() {
   isWindows = CC["@mozilla.org/xre/app-info;1"].
     getService(CI.nsIXULRuntime).OS == "WINNT";
@@ -55,9 +56,11 @@ function onLoad() {
   document.getElementById("proxynotes").value = proxy.notes;
   document.getElementById("animatedIcons").checked = proxy.animatedIcons;
   document.getElementById("cycleEnabled").checked = proxy.includeInCycle;
-  document.getElementById("clearCacheBeforeUse").checked = proxy.clearCacheBeforeUse;
+  document.getElementById("clearCacheBeforeUse").checked = proxy.
+    clearCacheBeforeUse;
   document.getElementById("disableCache").checked = proxy.disableCache;
-  document.getElementById("clearCookiesBeforeUse").checked = proxy.clearCookiesBeforeUse;
+  document.getElementById("clearCookiesBeforeUse").checked = proxy.
+    clearCookiesBeforeUse;
   document.getElementById("rejectCookies").checked = proxy.rejectCookies;
   document.getElementById("colorpicker").color = proxy.color;
   pickcolor(proxy.color); // NEW SVG
@@ -218,19 +221,24 @@ function onOK() {
 }
 
 function handleCacheAndCookies() {
-  fp = CC["@leahscape.org/foxyproxy/service;1"].getService().wrappedJSObject;
+  let fp = CC["@leahscape.org/foxyproxy/service;1"].getService().
+    wrappedJSObject;
   if (!fp._selectedProxy) return; // We're in patterns, random, or round-robin mode
 
   // If |proxy| is the currently selected ("in use") proxy, and the user changed
-  // the state of clearCacheBeforeUse from false to true, then we clear the cache now.
+  // the state of clearCacheBeforeUse from false to true, then we clear the
+  // cache now.
   let newState = document.getElementById("clearCacheBeforeUse").checked;
-  if (!proxy.disableCache && newState && fp._selectedProxy.id == proxy.id)
+  if (!proxy.clearCacheBeforeUse && newState && fp._selectedProxy.id ==
+      proxy.id)
     cacheMgr.clearCache();
   proxy.clearCacheBeforeUse = newState;
   // If |proxy| is the currently selected ("in use") proxy, and the user changed
-  // the state of clearCookiesBeforeUse from false to true, then we clear cookies now.
+  // the state of clearCookiesBeforeUse from false to true, then we clear
+  // cookies now.
   newState = document.getElementById("clearCookiesBeforeUse").checked;
-  if (!proxy.clearCookiesBeforeUse && newState && fp._selectedProxy.id == proxy.id)
+  if (!proxy.clearCookiesBeforeUse && newState && fp._selectedProxy.id ==
+      proxy.id)
     cookieMgr.clearCookies();
   proxy.clearCookiesBeforeUse = newState;
   // If |proxy| is the currently selected ("in use") proxy, and the user changed
@@ -239,9 +247,8 @@ function handleCacheAndCookies() {
   if (!proxy.disableCache && newState && fp._selectedProxy.id == proxy.id)
     cacheMgr.disableCache();
   proxy.disableCache = newState;
-
   // If |proxy| is the currently selected ("in use") proxy, and the user changed
-  // the state of disableCache from false to true, then we disable cache now.
+  // the state of rejectCookies from false to true, then we disable cookies now.
   newState = document.getElementById("rejectCookies").checked;
   if (!proxy.rejectCookies && newState && fp._selectedProxy.id == proxy.id)
     cookieMgr.rejectCookies();

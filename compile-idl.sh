c79bf3845449e460ac49c91e59bd973839c5f230
Eric Jung <eric.jung@getfoxyproxy.org>
2012-03-02 18:18:28 +0000

    updating idl compilation script to match new API directory structure

--- compile-idl.sh
+++ compile-idl.sh
@@ -2,7 +2,7 @@
 
 XULRUNNER_SDK_PATH=/home/fidel/dev/xulrunner-sdk
 CACHE_PATH=/tmp
-OUTPUT_PATH=/home/fidel/dev/foxyproxy/basic_and_standard/trunk/src/components
-INPUT_PATH=/home/fidel/dev/foxyproxy/basic_and_standard/trunk/src/components
+OUTPUT_PATH=/home/fidel/dev/foxyproxy/basic_and_standard/trunk/src/components/api
+INPUT_PATH=/home/fidel/dev/foxyproxy/basic_and_standard/trunk/src/components/api
 
 $XULRUNNER_SDK_PATH/sdk/bin/typelib.py -I $XULRUNNER_SDK_PATH/idl/ --cachedir $CACHE_PATH -o $OUTPUT_PATH/api.xpt $INPUT_PATH/api.idl

20a0c777a11a7a3652958fc3fc51c293c4ddc4a0
gkoppen <gkoppen@560d4a0b-fb2f-0410-bc46-b98b48829bb1>
2013-03-21 11:52:06 +0000

    fixed broken no internal IPs patterns
    git-svn-id: file:///opt/dev/work/svn/svn/basic_and_standard/trunk@1394 560d4a0b-fb2f-0410-bc46-b98b48829bb1

--- src/content/addeditproxy.js
+++ src/content/addeditproxy.js
@@ -284,9 +284,9 @@ function _checkUri() {
 
 function noInternalIPs() {
   let noInternalIPsChecked;
-  let localhostRegEx = "^https?://(?:[^:@/]+(?::[^@/]+)?@)?(?:localhost|127\\.\\d+\\.\\d+\\.\\d+)(?::\\d+)?.*";
-  let localSubRegEx = "^https?://(?:[^:@/]+(?::[^@/]+)?@)?(?:192\\.168\\.\\d+\\.\\d+|10\\.\\d+\\.\\d+\\.\\d+|172\\.(?:1[6789]|2[0-9]|3[01])\\.\\d+\\.\\d+)(?::\\d+)?.*";
-  let localHostnameRegEx = "^https?://(?:[^:@/]+(?::[^@/]+)?@)?[\\w-]+(?::\\d+)?.*";
+  let localhostRegEx = "^https?://(?:[^:@/]+(?::[^@/]+)?@)?(?:localhost|127\\.\\d+\\.\\d+\\.\\d+)(?::\\d+)?(?:/.*)?$";
+  let localSubRegEx = "^https?://(?:[^:@/]+(?::[^@/]+)?@)?(?:192\\.168\\.\\d+\\.\\d+|10\\.\\d+\\.\\d+\\.\\d+|172\\.(?:1[6789]|2[0-9]|3[01])\\.\\d+\\.\\d+)(?::\\d+)?(?:/.*)?$";
+  let localHostnameRegEx = "^https?://(?:[^:@/]+(?::[^@/]+)?@)?[\\w-]+(?::\\d+)?(?:/.*)?$";
   if (window.arguments[0].inn.torwiz) {
     noInternalIPsChecked = document.getElementById("fpniip").checked;
   } else {
